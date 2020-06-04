8b79d7dfdb98f34d4a06ab1fc3f1b59e148f845d
Vinicius Bernardino da Silva <viniciusbernardino1@hotmail.com>
2020-06-04 11:45:22 -0300

    initializing

---
+++ cmd/cyoaweb/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	filename := flag.String("file", "gopher.json", "the JSON file with the CYOA story")
+	flag.Parse()
+	fmt.Println(*filename)
+
+	f, err := os.Open(*filename)
+	if err != nil {
+		panic(err)
+	}
+
+	d := json.NewDecoder(f)
+	var story cyoa.Story
+	if err := d.Decode(&story); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("%+v\n", story)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/vncsb/choose-your-own-adventure
+
+go 1.14
